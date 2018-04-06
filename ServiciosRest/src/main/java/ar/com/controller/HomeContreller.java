60741309c6b68827447ade80b3bc264729c465fc
RuizMarceloAlejandro <ruiz.marcelo.a@gmail.com>
2018-04-06 10:36:46 -0300

    actualizo HomeContreller

--- ServiciosRest/src/main/java/ar/com/controller/HomeContreller.java
+++ ServiciosRest/src/main/java/ar/com/controller/HomeContreller.java
@@ -1,5 +1,14 @@
 package ar.com.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import ar.com.modelo.Persona;
+
+
 public class HomeContreller {
 
+	@RequestMapping(value="Persona", method = RequestMethod.GET)
+	public @ResponseBody Persona ObtenerPersona()
 }
