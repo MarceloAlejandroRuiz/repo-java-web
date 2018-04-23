ecd6ccf023abd06f694bb300a0f568147848f2fe
RuizMarceloAlejandro <ruiz.marcelo.a@gmail.com>
2018-04-23 13:01:30 -0300

    El servicio REST esta funcionando.

--- ServiciosRest/src/main/java/ar/com/controller/HomeContreller.java
+++ ServiciosRest/src/main/java/ar/com/controller/HomeContreller.java
@@ -3,12 +3,20 @@ package ar.com.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import ar.com.modelo.Persona;
 
-
+@RestController
+@RequestMapping("/service/greeting")
 public class HomeContreller {
 
-	@RequestMapping(value="Persona", method = RequestMethod.GET)
-	public @ResponseBody Persona ObtenerPersona()
+	@RequestMapping(value="/persona",method=RequestMethod.GET)
+	public  @ResponseBody Persona ObtenerPersona() {
+		Persona p = new Persona();
+		p.setApellido("ruiz");
+		p.setNombre("marcelo");
+		p.setEdad(23);
+		return  p;
+	}
 }
